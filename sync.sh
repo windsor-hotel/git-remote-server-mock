065106695bfe4f5ead37295e464e0d345c29fd6a
Ryan VanGundy <ryan.vangundy@gmail.com>
2023-11-25 20:03:30 -0500

    Refactor
    Signed-off-by: Ryan VanGundy <ryan.vangundy@gmail.com>

--- sync.sh
+++ sync.sh
@@ -3,48 +3,53 @@ set -eou pipefail
 
 RSYNC_EXCLUDE=${RSYNC_EXCLUDE:-}
 
-# Always exclude .git and expand other excludes from RSYNC_EXCLUDE variable
-exclude_args=('--exclude=.git')
-if [ -n "$RSYNC_EXCLUDE" ]; then
-  IFS=',' read -ra EXCLUDES <<< "$RSYNC_EXCLUDE"
-  for exclude in "${EXCLUDES[@]}"; do
-    exclude_args+=("--exclude=$exclude")
-  done
-fi
+# Function to handle exclusion arguments for rsync
+build_exclude_args() {
+  local exclude_args=('--exclude=.git')
+  if [ -n "$RSYNC_EXCLUDE" ]; then
+    IFS=',' read -ra EXCLUDES <<< "$RSYNC_EXCLUDE"
+    for exclude in "${EXCLUDES[@]}"; do
+      exclude_args+=("--exclude=$exclude")
+    done
+  fi
+  echo "${exclude_args[@]}"
+}
 
 # Function to handle syncing, committing, and pushing for a given repository
 handle_sync() {
-  repo_name="$1"
-  src_dir="/repos/mount/$repo_name"
-  work_dir="/repos/serve/$repo_name"
-  bare_repo_dir="/repos/git/$repo_name.git"
+  local repo_name="$1"
+  local src_dir="/repos/mount/$repo_name"
+  local work_dir="/repos/serve/$repo_name"
+  local bare_repo_dir="/repos/git/$repo_name.git"
+  local exclude_args
+  exclude_args=$(build_exclude_args)
 
-  cd "$work_dir" || exit
+  cd "$work_dir" || return
 
-  # Ensure the local repository tracks the remote repository
-  git remote add origin "file://$bare_repo_dir" || true
+  # Check if remote 'origin' exists, add if not
+  if ! git remote | grep -q origin; then
+    git remote add origin "file://$bare_repo_dir"
+  fi
+
+  # Fetch changes from the bare repository
   git fetch origin
 
-  # Reset the local state to match the remote
+  # Reset to the latest state of 'origin/main'
   git reset --hard origin/main
 
   # Sync the current state
   rsync -av --delete "${exclude_args[@]}" "$src_dir/" "$work_dir/"
 
+  # Stage changes
   git add .
 
   # Check if there are any changes to commit
   if ! git diff-index --quiet HEAD --; then
-    # Commit, pull and push the changes
+    # Commit and rebase with the latest changes
     timestamp=$(date "+%Y-%m-%d %H:%M:%S")
     git commit -m "Autocommit: $timestamp"
-    if git remote | grep -q origin; then
-      git pull origin main --rebase
-    else
-      git remote add origin "file://$bare_repo_dir"
-      git pull origin main --rebase
-    fi
-    git push "file://$bare_repo_dir" HEAD:refs/heads/main
+    git pull --rebase origin main
+    git push origin main
   fi
 }
 
